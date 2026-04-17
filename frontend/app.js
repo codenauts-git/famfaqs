71b169b636310ff8029fc258b5c869d7b183fa89
Jason Barnachea <jrbarnachea@gmail.com>
2026-04-17 15:02:34 +0800

    feat: default notification type to "Notification"

--- frontend/app.js
+++ frontend/app.js
@@ -588,7 +588,7 @@ const Carousel = {
 
       const type = item.type
         ? item.type.charAt(0).toUpperCase() + item.type.slice(1)
-        : "Nothing to show.";
+        : "Notification";
 
       row.innerHTML = `
         <div class="notification-bar"></div>
